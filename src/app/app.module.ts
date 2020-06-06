8073a836bd2baed4fab9fbf648d038d1410cb016
Anubhab Mukherjee <anubhabmukherjee5@gmail.com>
2020-06-06 19:04:19 +0530

    Added Required Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainBodyComponent } from './main-body/main-body.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainBodyComponent,
+    SideNavComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
